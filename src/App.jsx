5fcbbf6d94227b524f84810bc546c7bd8299a4b3
Shabeshvaran <shabeshvaran@gmail.com>
2025-06-29 01:31:30 +0530

    Styling navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import PageContent from './PageContent'
+import React from "react";
+import Navbar from "./components/Navbar";
+import PageContent from "./PageContent";
 
 const App = () => {
+  const [openNavigation, setOpenNavigation] = useState(false);
   return (
     <div className="bg-gradient-to-r from-gray-700 via-gray-900 to-black text-white min-h-screen overflow-hidden pt-2rem lg:pt-4.5rem">
-    <Navbar/>
-    <PageContent/>
+      <Navbar
+        openNavigation={openNavigation}
+        setOpenNavigation={setOpenNavigation}
+      />
+      <PageContent openNavigation={openNavigation} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
+// ??????????????????
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,167 @@
 import React, { useState } from "react";
 import { CgMenu } from "react-icons/cg";
+import { useLocation } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
 const Navbar = () => {
+  const pathname = useLocation();
 
-const [openNavigation, setOpenNavigation] = useState(false);
+  const [openNavigation, setOpenNavigation] = useState(false);
 
-const toggleNavigation = () => {
-  setOpenNavigation(!openNavigation)
-}
+  const toggleNavigation = () => {
+    if (openNavigation) {
+      setOpenNavigation(false);
+      enablePageScroll();
+    } else {
+      setOpenNavigation(true);
+      disablePageScroll();
+    }
+  };
+
+  const handleClick = () =>{
+      if(!openNavigation) return;
+      enablePageScroll()
+      setOpenNavigation(false)
+    }
 
   return (
     // bg-[#FFFFFF30] - old bg
-    <header className="fixed top-0 left-0 w-full z-50 bg-opacity-70 backdrop-blur-md">
+    <header className="fixed top-0 left-0 w-full z-50 bg-gradient-to-r from-gray-700 via-gray-900 to-black backdrop-blur-md">
       <nav className="flex justify-between items-center px-4 h-16 lg:">
-        <a href="#home" className="text-2xl font-semibold">ABSOLUTE CINEMA</a>
-        <div className={`absolute top-[64px]  left-0 w-full py-2 text-lg flex-col gap-6 items-center ${openNavigation ? 'flex' : 'hidden'} lg:flex lg:flex-row lg:static lg:w-auto lg:bg-transparent`}>
-          <ul className="flex flex-col gap-6 items-center lg:flex-row lg:gap-8">
-            <a href="#home"><li className="hover:cursor-pointer">Home</li></a>
-            <a href="#random"><li className="hover:cursor-pointer">Random</li></a>
-            <a href="#guide"><li className="hover:cursor-pointer">Guide</li></a>
-            <a href="#search"><li className="hover:cursor-pointer">Search</li></a>
-            <a href="#about"><li className="hover:cursor-pointer">About</li></a>
+        <a
+          href="#home"
+          className="text-2xl font-semibold font-serif font-stretch-extra-condensed hover:text-green-500"
+        >
+          ABSOLUTE CINEMA.
+        </a>
+        <div
+          className={`absolute top-[64px] left-0 w-full py-2 text-lg flex-col gap-6 items-center ${
+            openNavigation ? "flex" : "hidden"
+          } lg:flex lg:flex-row lg:static lg:w-auto lg:bg-transparent`}
+        >
+          <ul className="flex flex-col gap-6 items-center lg:flex-row lg:gap-8 ">
+            <a href="#home" onClick={handleClick}>
+              <li className="hover:cursor-pointer hover:text-green-500 font-semibold">
+                {pathname.hash === "#home" ? (
+                  <span className="relative inline-block text-green-500">
+                    Home
+                    <svg
+                      className="absolute -bottom-0.5 left-0 w-full max-h-1.5"
+                      viewBox="0 0 55 5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      preserveAspectRatio="none"
+                    >
+                      <path
+                        d="M0.652466 4.00002C15.8925 2.66668 48.0351 0.400018 54.6853 2.00002"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                      ></path>
+                    </svg>
+                  </span>
+                ) : (
+                  "Home"
+                )}
+              </li>
+            </a>
+            <a href="#random" onClick={handleClick}>
+              <li className="hover:cursor-pointer hover:text-green-500 font-semibold">
+                {pathname.hash === "#random" ? (
+                  <span className="relative inline-block text-green-500">
+                    Random
+                    <svg
+                      className="absolute -bottom-0.5 left-0 w-full max-h-1.5"
+                      viewBox="0 0 55 5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      preserveAspectRatio="none"
+                    >
+                      <path
+                        d="M0.652466 4.00002C15.8925 2.66668 48.0351 0.400018 54.6853 2.00002"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                      ></path>
+                    </svg>
+                  </span>
+                ) : (
+                  "Random"
+                )}
+              </li>
+            </a>
+            <a href="#guide" onClick={handleClick}>
+              <li className="hover:cursor-pointer hover:text-green-500 font-semibold">
+                {pathname.hash === "#guide" ? (
+                  <span className="relative inline-block text-green-500">
+                    Guide
+                    <svg
+                      className="absolute -bottom-0.5 left-0 w-full max-h-1.5"
+                      viewBox="0 0 55 5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      preserveAspectRatio="none"
+                    >
+                      <path
+                        d="M0.652466 4.00002C15.8925 2.66668 48.0351 0.400018 54.6853 2.00002"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                      ></path>
+                    </svg>
+                  </span>
+                ) : (
+                  "Guide"
+                )}
+              </li>
+            </a>
+            <a href="#search" onClick={handleClick}>
+              <li className="hover:cursor-pointer hover:text-green-500 font-semibold">
+                {pathname.hash === "#search" ? (
+                  <span className="relative inline-block text-green-500">
+                    Search
+                    <svg
+                      className="absolute -bottom-0.5 left-0 w-full max-h-1.5"
+                      viewBox="0 0 55 5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      preserveAspectRatio="none"
+                    >
+                      <path
+                        d="M0.652466 4.00002C15.8925 2.66668 48.0351 0.400018 54.6853 2.00002"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                      ></path>
+                    </svg>
+                  </span>
+                ) : (
+                  "Search"
+                )}
+              </li>
+            </a>
+            <a href="#about" onClick={handleClick}>
+              <li className="hover:cursor-pointer hover:text-green-500 font-semibold">
+                {pathname.hash === "#about" ? (
+                  <span className="relative inline-block text-green-500">
+                    About
+                    <svg
+                      className="absolute -bottom-0.5 left-0 w-full max-h-1.5"
+                      viewBox="0 0 55 5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      preserveAspectRatio="none"
+                    >
+                      <path
+                        d="M0.652466 4.00002C15.8925 2.66668 48.0351 0.400018 54.6853 2.00002"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                      ></path>
+                    </svg>
+                  </span>
+                ) : (
+                  "About"
+                )}
+              </li>
+            </a>
           </ul>
         </div>
         <div onClick={toggleNavigation} className="lg:hidden">
-          <CgMenu className={`text-2xl hover:cursor-pointer`}/>
+          <CgMenu className={`text-2xl hover:cursor-pointer`} />
         </div>
       </nav>
-    </header> 
+    </header>
   );
 };
 
