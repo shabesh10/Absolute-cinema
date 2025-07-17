838cfe31628df88a190d801586849557b18d4d5f
Shabeshvaran <shabeshvaran@gmail.com>
2025-07-17 18:13:29 +0530

    added images in sliders

--- src/sections/Guide.jsx
+++ src/sections/Guide.jsx
@@ -3,42 +3,40 @@ import Slide from "./Slide";
 import { BsChevronCompactLeft } from "react-icons/bs";
 import { BsChevronCompactRight } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
+let data_arr = [
+  {
+    title: "IMDB Top 250",
+    desc: "Explore the highest-rated movies of all time as voted by millions on IMDb. A perfect starting point for classic and critically acclaimed films.",
+    imageUrl: "https://images.unsplash.com/photo-1729860649288-73dc62463c71?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "https://www.imdb.com/chart/top/",
+  },
+  {
+    title: "Letterboxd Top 250",
+    desc: "Curated by the most active cinephiles online, this list highlights beloved and rewatchable films with strong cult followings.",
+    imageUrl: "src\\assets\\letterboxd-rating.webp",
+    url: "https://letterboxd.com/jack/list/official-top-250-films-with-the-most-fans/",
+  },
+  {
+    title: "Favourite Director Binge",
+    desc: "Dive deep into the works of your favorite directors — from Nolan to Fincher — and experience their storytelling evolution.",
+    imageUrl: "https://images.unsplash.com/photo-1577190651915-bf62d54d5b36?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 
+    url: "",
+  },
+  {
+    title: "Franchise Binge",
+    desc: "Get lost in expansive cinematic universes — Marvel, Harry Potter, LOTR, James Bond, Star wars and more — in back-to-back marathons.",
+    imageUrl: "https://images.unsplash.com/photo-1612036782180-6f0b6cd846fe?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "",
+  },
+  {
+    title: "Genre Binge",
+    desc: "You must have a favourite genre, binge watch top films from that genre.",
+    imageUrl: "https://images.unsplash.com/photo-1548754218-e9ef33578d04?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    url: "",
+  },
+];
 const Guide = () => {
-  let data_arr = [
-    {
-      title: "IMDB Top 250",
-      desc: "Explore the highest-rated movies of all time as voted by millions on IMDb. A perfect starting point for classic and critically acclaimed films.",
-      imageUrl: "",
-      url: "https://www.imdb.com/chart/top/",
-    },
-    {
-      title: "Letterboxd Top 250",
-      desc: "Curated by the most active cinephiles online, this list highlights beloved and rewatchable films with strong cult followings.",
-      imageUrl: "",
-      url: "https://letterboxd.com/jack/list/official-top-250-films-with-the-most-fans/",
-    },
-    {
-      title: "Favourite Director Binge",
-      desc: "Dive deep into the works of your favorite directors — from Nolan to Tarantino — and experience their storytelling evolution.",
-      imageUrl: "",
-
-      url: "",
-    },
-    {
-      title: "Franchise Binge",
-      desc: "Get lost in expansive cinematic universes — Marvel, Harry Potter, LOTR, and more — in back-to-back marathons.",
-      imageUrl: "",
-      url: "",
-    },
-    {
-      title: "Genre Binge",
-      desc: "Craving horror? Romance? Sci-fi? This guide lets you pick a genre and go all-in with the most iconic entries.",
-      imageUrl: "",
-      url: "",
-    },
-  ];
-
   // Render the guide section with slides
   // Each slide will display a title, description, image, and an optional link
   const [curridx, setCurrIdx] = useState(0);
@@ -51,7 +49,10 @@ const Guide = () => {
   const nextSlide = () => {
     const isLastSlide = curridx === data_arr.length - 1;
     const newidx = isLastSlide ? 0 : curridx + 1;
-    setCurrIdx(newidx); 
+    setCurrIdx(newidx);
+  };
+  const dotUpdater = (idx) => {
+    setCurrIdx(idx);
   };
 
   return (
@@ -66,20 +67,24 @@ const Guide = () => {
           />
         </div>
         <div
-          className="absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
+          className="absolute top-[50%] -translate-x-0 translate-y-[-140px] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
           onClick={prevSlide}
         >
           <BsChevronCompactLeft size={30} />
         </div>
         <div
-          className="absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
+          className="absolute top-[50%] -translate-x-0 translate-y-[-140px] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
           onClick={nextSlide}
         >
           <BsChevronCompactRight size={30} />
         </div>
-        <div className="flex top-4 justify-center py-2">
+        <div className="absolute flex bottom-60">
           {data_arr.map((slide, index) => (
-            <div key={index} className="text-2xl cursor-pointer hover:text-3xl" onClick={() => setCurrIdx(index)}>
+            <div
+              key={index}
+              className={`text-2xl cursor-pointer hover:text-3xl hover:text-white text-gray-400` + (curridx === index ? "text-white text-3xl" : "")}
+              onClick={() => dotUpdater(index)}
+            >
               <RxDotFilled />
             </div>
           ))}
--- src/sections/Slide.jsx
+++ src/sections/Slide.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 
-const Slide = (props) => {
-  
+const Slide = ({ title = "Untitled", desc = "", imageUrl = "", url = "" }) => {
   return (
-    <div className="flex flex-col text-center">
-      <img src={props.imageUrl} alt={`${props.title} related image`} />
-      {props.url.length > 0 ? (
-        <a href={props.url} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline hover:cursor-pointer">
-          <h2 className="text-xl font-bold mb-4">{props.title}</h2>
-        </a>
-      ) : (
-        <h2 className="text-xl font-bold mb-4">{props.title}</h2>
-      )}
-      <p>{props.desc}</p>
+    <div className="flex flex-col items-center text-center bg-slate-800 text-white p-6 rounded-xl shadow-lg transition-all duration-300">
+      <img
+        src={imageUrl || "src\\assets\\letterboxd-rating.webp"}
+        alt={`${title} related image`}
+        className="w-full max-h-[400px] max-w-[600px] object-cover rounded-xl mb-4 aspect-[16/9]"
+      />
+      <h2 className="text-xl font-bold mb-4">
+        {url ? (
+          <a
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-blue-400 hover:underline"
+          >
+            {title}
+          </a>
+        ) : (
+          title
+        )}
+      </h2>
+      <p>{desc}</p>
     </div>
   );
 };
